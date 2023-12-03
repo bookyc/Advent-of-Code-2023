5d926e0358058807bfa8158391aa4fb30c96d3c3
bookyc <139672499+bookyc@users.noreply.github.com>
2023-12-03 14:01:59 -0700

    Advent of code day 1 solutions (includes input)

---
+++ advent_01_part1.py
@@ -0,0 +1,25 @@
+def find_values(line: str) -> str:
+    for i in reversed(range(len(line))):
+        if line[i].isdigit():
+            num1 = line[i]
+    for i in range(len(line)):
+        if line[i].isdigit():
+            num2 = line[i]
+    return num1 + num2
+
+def sum_of_values(calibration_values: list[str]) -> int:
+    sum = 0
+    for i in calibration_values:
+        sum += int(i)
+    return sum
+
+def main() -> None:
+    input = open("day_one_input.txt", "r")
+    calibration_values = []
+    for i in input:
+        calibration_values.append(find_values(i))
+
+    print(sum_of_values(calibration_values))
+    input.close()
+
+main()
